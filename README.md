5b7d89d17226646dddf4b940e824fe66eb491885
ZFan <lzf368@hotmail.com>
2016-10-21 06:57:44 +0800

    Add Bootstrap support

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # first blog proj
+
+#from flask import Flask, render_template
+#from config import Configuration
+
+#from flask_bootstrap import Bootstrap
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
-from config import Configuration  
+from config import Configuration
+
+from flask_bootstrap import Bootstrap  # Make Bootstrap support 
 
 app = Flask(__name__)
 
 app.config.from_object(Configuration)  # read configuration from config
+
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
